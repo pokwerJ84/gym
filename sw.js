ee321bb6f9f47ebf91cb4b7da16ac727f5d80ad3
pokwerJ84 <pokwer1984@gmail.com>
2026-08-16 21:21:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const VERSION="v97";
+const VERSION="v98";
 const CORE_CACHE=`gym-tracker-core-${VERSION}`;
 const IMAGE_CACHE=`gym-tracker-supabase-images-${VERSION}`;
-const IMAGE_PREFIX=new URL("./exercise-guides/v97/",self.location.href).href;
+const IMAGE_PREFIX="https://wknfwqjuatnozjksqutf.supabase.co/storage/v1/object/public/exercise-guides/v97/";
 const CORE_ASSETS=[
   "./",
   "./index.html",
